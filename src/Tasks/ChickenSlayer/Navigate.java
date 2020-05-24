5f71b2b0e95a77e9e1edd917713811783b885f4a
Doshi <doshi@Doshis-MacBook-Pro.local>
2020-05-24 13:19:22 -0400

    adding chicken slayer script and tasks

---
+++ src/Tasks/ChickenSlayer/Navigate.java
@@ -0,0 +1,23 @@
+package Tasks.ChickenSlayer;
+
+import Tasks.Task;
+import org.powerbot.script.rt4.ClientContext;
+
+public class Navigate extends Task<ClientContext> {
+
+
+    public Navigate(ClientContext ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public boolean activate() {
+
+        return false;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
--- src/scripts/ChickenSlayer.java
+++ src/scripts/ChickenSlayer.java
@@ -3,11 +3,15 @@ package scripts;
 
 import Tasks.Task;
 import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
 import org.powerbot.script.rt4.ClientContext;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+
+@Script.Manifest(name = "Chicken Slayer", description = "Kills the chicken in lumbridge", properties = "client=4; topic=0;")
 
 public class ChickenSlayer extends PollingScript<ClientContext> {
 
